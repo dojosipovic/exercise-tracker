f15c2278aac8af0e443a1a494c30507953210a8c
Dominik Josipović <dominik.josipovic@gmail.com>
2024-03-01 13:53:24 +0000

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
 require('dotenv').config()
 
 app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
+});
+
+let users = [];
+
+app.post('/api/users', (req, res) => {
+  let username = req.body.username;
+  let _id = users.length + 1;
+  let user = { username: username, _id: _id};
+  users.push(user);
+
+  res.json(user);
 });
 
 
